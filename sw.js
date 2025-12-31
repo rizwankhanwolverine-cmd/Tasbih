38f0ecb3657f128d3701574d46e1444c6d5c1eff
rizwankhanwolverine-cmd <rizwankhan.wolverine@gmail.com>
2025-12-31 14:11:34 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,31 @@
-const CACHE_NAME = 'sanctuary-v2';
+const CACHE_NAME = 'sanctuary-v3';
 const ASSETS = [
-  './',
   './index.html',
   './tasbih.html',
   './salah.html',
   './recite.html',
   './journey.html',
-  './manifest.json'
+  './manifest.json',
+  './icon.png'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
