355b7a3da3afc87b2528b7c99c677e05ea0097ec
rizwankhanwolverine-cmd <rizwankhan.wolverine@gmail.com>
2025-12-31 14:01:55 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,22 @@
-// FILE NAME: sw.js
-const CACHE_NAME = 'sanctuary-v1';
+const CACHE_NAME = 'sanctuary-v2';
 const ASSETS = [
   './',
   './index.html',
   './tasbih.html',
   './salah.html',
   './recite.html',
-  './journey.html'
+  './journey.html',
+  './manifest.json'
 ];
 
-// Install Event: Caches the App Shell and Modules
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Fetch Event: Serves content from Cache first (Instant Load)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
